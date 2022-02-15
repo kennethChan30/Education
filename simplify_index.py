f0c62a253a5f2caed90a65f7233cf3154f88694c
kennethChan30 <80766472+kennethChan30@users.noreply.github.com>
2022-02-16 07:06:46 +0900

    Deal with negative index

--- simplify_index.py
+++ simplify_index.py
@@ -3,13 +3,8 @@ import re
 def collect_term(question):
     #define expression of index form
     index = re.compile(r'[a-z]\^?-?\d*')
-    #seperate each index
-    nominator = sorted(index.findall(question))
-    #collec the same term of expression using dictonary
+    nominator = index.findall(question)
     ans = {}
-    #the ouput text
-    ans_string = ""
-    #loop all index
     for n in nominator:
         if n[0] not in ans.keys():
             if len(n) == 1:
@@ -21,13 +16,42 @@ def collect_term(question):
                 ans[n] = ans[n] + 1
             else:
                 ans[n[0]] = ans[n[0]] + int(n[2:])
-    #produce print answers
-    for k, v in ans.items():
-        if v == 1:
-            ans_string = ans_string + k
+    return ans
+
+def positive_index(step):
+    ans_string = ''
+    ans_nominator = {}
+    ans_denominator = {}
+#classify nominaort and denominator by the negative sign of power
+    for k, v in step.items():
+        if v < 0:
+            ans_denominator[k] = -v
         else:
-            ans_string = ans_string + k + '^' + str(v)
+            ans_nominator[k] = v
+#transform dictionaries to strings
+    nominator_string = collect_strings(ans_nominator)
+    denominator_string = collect_strings(ans_denominator)
+    if nominator_string == None and denominator_string == None:
+        ans_string = '1'
+    elif denominator_string == None:
+        ans_string = nominator_string
+    elif nominator_string == None:
+        ans_string = '1/' + denominator_string
+    else:
+        ans_string = nominator_string + '/' + denominator_string
     return ans_string
 
+def collect_strings(step):
+    collect_string = ""
+    for k, v in step.items():
+        if v == 0:
+            continue
+        elif v == 1:
+            collect_string = collect_string + k + ' '
+        else:
+            collect_string = collect_string + k + '^' + str(v) + ' '
+    if len(collect_string) == 0:
+        return None
+    return collect_string
 def simplify_index(question):
-    return collect_term(question)
+    return positive_index(collect_term(question))
